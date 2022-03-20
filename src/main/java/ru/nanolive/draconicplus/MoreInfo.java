be2302a8e841b0ac50b40b1444cdcdbf91985598
Asek3 <bogban.zp@gmail.com>
2022-03-20 12:33:38 +0200

    Bump Version

--- src/main/java/ru/nanolive/draconicplus/MoreInfo.java
+++ src/main/java/ru/nanolive/draconicplus/MoreInfo.java
@@ -4,7 +4,7 @@ public class MoreInfo {
 
 	public static final String MODID = "draconicplus";
     public static final String NAME = "DraconicMinus-";
-    public static final String VERSION = "1.0R";
+    public static final String VERSION = "1.1R";
     public static final String SERVERPROXY = "ru.nanolive.draconicplus.proxy.CommonProxy";
     public static final String CLIENTPROXY = "ru.nanolive.draconicplus.proxy.ClientProxy";
 }
